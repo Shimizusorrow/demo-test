c04c951bb36f2b1d8a1005db2d72d27cb88847f0
shimizu <shimizusorrow>
2021-04-12 10:03:18 +0800

    新增 workspace 逻辑分区 基于admin的拓展逻辑区域

---
+++ workspace/src/main/java/com/unfrost/workspace/WorkspaceApplication.java
@@ -0,0 +1,17 @@
+package com.unfrost.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+        "com.unfrost.common",
+        "com.unfrost.admin",
+        "com.unfrost.workspace"
+})
+public class WorkspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkspaceApplication.class, args);
+    }
+
+}
